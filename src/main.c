c52f92154b0a6cdf3cd967e2ffa64658af91158a
Maleehaakbar <maleehaashley@gmail.com>
2026-01-21 16:21:22 +0500

    add external module

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include <zephyr/kernel.h>
+#ifdef CONFIG_ENABLE_TEST
+#include "test.h"
+#endif
+#define SLEEP_TIME_MS 100U
 
 int main(void)
 {
+       
+        
+        while(1)
+        {
+                printk("helloworld\n");
+                k_msleep(SLEEP_TIME_MS);
+                
+                #ifdef CONFIG_ENABLE_TEST
+                int sum = CONFIG_NUM_A + CONFIG_NUM_B;
+                printk("sum is %d", sum);
+                k_msleep(SLEEP_TIME_MS);
+                #endif
+        }
+        
         return 0;
+
 }
